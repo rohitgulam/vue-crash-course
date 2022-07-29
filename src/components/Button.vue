c5964df9ab9aaa3cfa193f92807f7eecac2b45aa
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-29 21:26:18 +0300

    created button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="onClick()" :style="{background: color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Button',
+    props: {
+        text: String,
+        color: String
+    },
+    methods: {
+        onClick(){
+            console.log('Btn CLick');
+        },
+    }
+}
+</script>
