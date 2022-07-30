541c8865f82de2670871829f0a81497ea63ba382
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-30 19:24:40 +0300

    added add task

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="container">
-    <HeaderItem title="Task Tracker" />
+    <HeaderItem 
+    @toggle-add-task="toggleAddTask" 
+    title="Task Tracker" 
+    :showAddTask="showAddTask" />
+    <div v-if="showAddTask" >
+      <AddTask @add-task="addTask" />
+    </div>
     <TasksItem @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks"/>
   </div>
 </template>
@@ -8,20 +14,29 @@
 <script>
 import HeaderItem from './components/Header'
 import TasksItem from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 export default {
   name: 'App',
   components: {
     HeaderItem,
-    TasksItem
+    TasksItem,
+    AddTask
   },
   data(){
     return{
-      tasks: []
+      tasks: [],
+      showAddTask: false,
     }
   },
   methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
+    addTask(task){
+      this.tasks = [...this.tasks, task]
+    },
     deleteTask(id){
       this.tasks = this.tasks.filter((task) => {
         task.id != id
