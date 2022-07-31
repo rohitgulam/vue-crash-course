fc7cb5afc4a8d8a037c14bea22c97ad0da6ef8ce
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-31 10:37:13 +0300

    created footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+    <footer>
+        <p>Copyright &copy; 2022</p>
+        <a href="/about">About</a>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterIten',
+}
+</script>
+
+<style scoped>
+    a{
+        color: #333;
+    }
+
+    footer{
+        margin-top: 30px;
+        text-align: center;
+    }
+</style>
