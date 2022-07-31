ce13fffae2d993b961e89cad44f62ad57b2cae57
Rohit Gulam <rohitsgulam@gmail.com>
2022-07-31 10:11:45 +0300

    added proxy for api

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    proxy: {
+      '^/api':{
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: {'^/api' : '/'},
+      },
+    }
+  }
 })
